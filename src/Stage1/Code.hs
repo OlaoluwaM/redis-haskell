177a5228fe514871085539effcad99be04290d3a
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2024-07-31 21:43:03 -0500

    feat: Organize project structure

---
+++ src/Stage1/Code.hs
@@ -0,0 +1,15 @@
+module Stage1.Code (simpleServer) where
+
+import Network.Simple.TCP (HostPreference (HostAny), closeSock, serve)
+
+simpleServer :: IO ()
+simpleServer = do
+    -- You can use print statements as follows for debugging, they'll be visible when running tests.
+    putStrLn "Logs from your program will appear here"
+
+    -- Uncomment this block to pass stage 1
+    let port = "6379"
+    putStrLn $ "Redis server listening on port " ++ port
+    serve HostAny port $ \(socket, address) -> do
+        putStrLn $ "successfully connected client: " ++ show address
+        closeSock socket
