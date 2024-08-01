d51c4fa62459f9c15941017276989b221c6f97ed
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2024-07-31 21:53:34 -0500

    fix: Bump down haskell and snapshot version..again

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main (main) where
 
-import Stage1.Code (simpleServer)
+import Network.Simple.TCP (HostPreference (HostAny), closeSock, serve)
 
 main :: IO ()
-main = simpleServer
+main = do
+    -- You can use print statements as follows for debugging, they'll be visible when running tests.
+    putStrLn "Logs from your program will appear here"
+
+    -- Uncomment this block to pass stage 1
+    let port = "6379"
+    putStrLn $ "Redis server listening on port " ++ port
+    serve HostAny port $ \(socket, address) -> do
+        putStrLn $ "successfully connected client: " ++ show address
+        closeSock socket
