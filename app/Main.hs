eb6c676c0dc59fdb52dc71012a65778eec4edff0
Olaoluwa Mustapha <37044906+OlaoluwaM@users.noreply.github.com>
2024-07-31 22:07:32 -0500

    fix: Bump up snapshot version ghc 9.4.8

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,6 @@
 module Main (main) where
 
-import Network.Simple.TCP (HostPreference (HostAny), closeSock, serve)
+import Stage1.Code (simpleServer)
 
 main :: IO ()
-main = do
-    -- You can use print statements as follows for debugging, they'll be visible when running tests.
-    putStrLn "Logs from your program will appear here"
-
-    -- Uncomment this block to pass stage 1
-    let port = "6379"
-    putStrLn $ "Redis server listening on port " ++ port
-    serve HostAny port $ \(socket, address) -> do
-        putStrLn $ "successfully connected client: " ++ show address
-        closeSock socket
+main = simpleServer
